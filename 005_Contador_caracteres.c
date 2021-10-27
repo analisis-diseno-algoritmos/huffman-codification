76da4724c3aebb31a2fd9f81d60ba832ba6d21c0
DanAurMG <raizeremi.15.damg@gmail.com>
2021-10-26 23:48:31 -0500

    Inten. contarcaracteres luego arreglo los archivos

--- 005_Contador_caracteres.c
+++ 005_Contador_caracteres.c
@@ -4,11 +4,14 @@
 //Programa para contabilizar determinados caracteres en un archivo de texto
 //Se que es muy pronto para utilizar apuntadores, pero utilizando el cierre del programa se presentaban fallos, por lo tanto, recurrí al uso de apuntadores y funciones sencillas
 
-int main(){
-    FILE *f = fopen("Prueba.txt", "r");
-    int caracteres = 0, espacios = 0, vocales = 0, lineas = 0, tabuladores = 0,c;
+int main(int argc, char const *argv[]){
+    FILE *f = fopen(argv[1], "r");
+    //FILE *f = fopen("Prueba.txt", "r");
+    int caracteres = 0, espacios = 0, vocales = 0, lineas = 0, tabuladores = 0;
+    char c;
     while (c != EOF){
         c = getc(f);
+        putchar(c);
         caracteres++;
         if(c == ' ')
             espacios++;
@@ -19,12 +22,13 @@ int main(){
         if(c == '\t')
             tabuladores++;
     }
-    printf("Caracteres ingresados: %d \n", caracteres);
+    //printf("%s \n", c);
+    printf("\nCaracteres ingresados: %d \n", caracteres);
     printf("Los espacios ingresados fueron: %d \n", espacios);
     printf("La cantidad de vocales ingresados fue: %d \n", vocales);
     printf("El total de lineas fue: %d \n", lineas);
     printf("Se introdujeron %d tabuladores \n", tabuladores);
-    fclose(f);
-    getchar();
+    putchar(c);
+    fclose(f);    
     return 0;
 }
